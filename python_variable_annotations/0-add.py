f6378880f68c68d2568cad9fddf32972159e476d
Alurae <10475@holbertonstudents.com>
2025-07-28 11:37:27 +0200

    Add initial implementation of the add function for float summation

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module 0-add
+Ce module contient une fonction qui retourne
+la somme de deux nombres à virgule flottante (float).
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Compute the sum of two float numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of `a` and `b`.
+    """
+    return a + b
