b38cf5af6573166e1495aa43db0979c4530b1431
Alurae <10475@holbertonstudents.com>
2025-07-28 16:48:39 +0200

    Refactor module to implement concurrent coroutine execution with wait_n function

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 """
-Module 0-basic_async_syntax
+Module 1-concurrent_coroutines
 
-This module contains a coroutine that waits for a random delay
-between 0 and max_delay seconds and returns the delay.
+This module defines an async function that spawns multiple
+wait_random coroutines concurrently and returns their results
+in ascending order (based on completion time).
 """
-import random
 import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """function"""
-    delai = random.uniform(0, max_delay)
-    await asyncio.sleep(delai)
-    return (delai)
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times and returns delays in order of completion."""
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    results = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        results.append(delay)
+    return results
