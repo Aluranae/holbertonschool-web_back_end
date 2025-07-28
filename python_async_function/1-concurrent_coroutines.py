ffa776a95233aebd3d546858294af7ce092c9ca2
Alurae <10475@holbertonstudents.com>
2025-07-28 16:49:47 +0200

    Refactor wait_n function to use asyncio.as_completed for ordered results

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Function"""
+    """Spawns wait_random n times and returns delays in order of completion."""
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    results = await asyncio.gather(*tasks)
-    return (results)
+    results = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        results.append(delay)
+    return results
