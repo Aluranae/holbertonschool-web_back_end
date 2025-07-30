1cf81cc30cebcc1a670f008bfe4a0a1eb37ec19e
Alurae <10475@holbertonstudents.com>
2025-07-30 08:46:28 +0200

    Fix async_generator type hint to include 'None' for the third parameter

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 This module defines an asynchronous generator that yields
 10 random numbers between 0 and 10 with a delay of 1 second between each.
@@ -9,7 +8,7 @@ import random
 import typing
 
 
-async def async_generator() -> typing.AsyncGenerator[float, None]:
+async def async_generator() -> typing.AsyncGenerator[float, None, None]:
     """Asynchronous generator that yields 10 random numbers."""
     for _ in range(10):
         await asyncio.sleep(1)
