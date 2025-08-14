4eef890ab8db7f06f0690e4dd342b1706b48d40f
Alurae <10475@holbertonstudents.com>
2025-08-14 17:07:57 +0200

    Replace count_documents with estimated_document_count for log counting in log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,15 +7,19 @@ about Nginx logs stored in the 'logs.nginx' collection.
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
-    print(collection.count_documents({}), "logs")
+    # Use estimated_document_count instead of count_documents({})
+    log_count = collection.estimated_document_count()
+    print(f"{log_count} logs")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        print(f"\tmethod {method}: {collection.count_documents({'method': method})}")
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    print(collection.count_documents({"method": "GET", "path": "/status"}), "status check")
+    # Status check (GET and path = /status)
+    status_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
