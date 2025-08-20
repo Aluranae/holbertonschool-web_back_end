71840a481977473fe66c0d42974000b0bdd5067e
Alurae <10475@holbertonstudents.com>
2025-08-20 15:47:25 +0200

    fix: correct variable name in students validation to improve clarity

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -35,7 +35,7 @@ export default class HolbertonCourse {
     if (!Array.isArray(value)) {
       throw new TypeError('Students must be an array');
     }
-    if (!value.every((students) => typeof students === 'string')) {
+    if (!value.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
