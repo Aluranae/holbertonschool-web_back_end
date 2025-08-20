36e7224f1e0a42f3d58cfa2e4113628ed9c9c6bc
Alurae <10475@holbertonstudents.com>
2025-08-20 16:25:23 +0200

    fix: use setter for floors property in SkyHighBuilding constructor and update evacuation message

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,21 +3,26 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(val) {
+    if (typeof val === 'number') this._sqft = val;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
-    this._floors = value;
+  set floors(val) {
+    if (typeof val === 'number') this._floors = val;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
