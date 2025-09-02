8f42d0c23771d2d5000cd36435a90334e60f1438
Aluranae <10475@holbertonstudents.com>
2025-09-02 15:03:51 +0200

    feat: add console display and stdin interaction for user input

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+// Le programme parle à l'utilisateur
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+// Écoute des réponses envoyées par l'utilisateur (stdin)
+process.stdin.on('data', (input) => {
+  const userName = input.toString().trim();
+  process.stdout.write(`Your name is: ${userName}\n`);
+});
+
+// Lorsque le flux est terminé (Ctrl+D par exemple)
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
